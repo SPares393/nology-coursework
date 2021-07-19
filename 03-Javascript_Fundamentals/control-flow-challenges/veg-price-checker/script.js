d97534077a94b5f3d1d424949677da91567b345f
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-07-19 10:53:15 +0100

    control flow code-along challenges

---
+++ 03-Javascript_Fundamentals/control-flow-challenges/veg-price-checker/script.js
@@ -0,0 +1,14 @@
+let vegtable;
+
+switch (vegtable) {
+  case "potato":
+  case "carrot":
+    console.log("£1.00 per kilo");
+    break;
+  case "broccoli":
+    console.log("£1.25 per kilo");
+    break;
+  case "cabbage":
+    console.log("£1.50 per kilo");
+    break;
+}
