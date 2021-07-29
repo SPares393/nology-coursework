551147d42e5409fab40248552c110f3cf1d5acc5
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-07-29 16:34:59 +0100

    two input functionality / added number buttons

--- 03-Javascript_Fundamentals/calculator-project/script.js
+++ 03-Javascript_Fundamentals/calculator-project/script.js
@@ -1,35 +1,49 @@
 const calculateAnswer = document.querySelector(".calculator-answer");
-const inputNumber1 = document.querySelector(".inputNumber1");
-const inputNumber2 = document.querySelector(".inputNumber2");
-const inputOperator = document.querySelector(".inputOperator");
+const inputNumber1 = document.querySelector(".calculator-input-1");
+const inputNumber2 = document.querySelector(".calculator-input-2");
+const currentOperator = document.querySelector(".current-operator");
+const operator = document.querySelectorAll(".operator");
 const calculatorReset = document.querySelector(".calculator-reset");
 const calculatorOutput = document.querySelector(".calculator-output");
+
+let displayOperator = "?";
+
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", () => {
+    displayOperator = operator[i].textContent;
+    currentOperator.textContent = displayOperator;
+  });
+}
 
 calculateAnswer.addEventListener("click", () => {
   const number1 = Number(inputNumber1.value);
   const number2 = Number(inputNumber2.value);
-  const operator = inputOperator.value;
+  const operator = currentOperator.value;
 
-  if (!number1 || !number2 || !operator) {
-    calculatorOutput.textContent = "Please enter a calculation";
-  } else if (operator == "+") {
+  calculatorOutput.textContent = "Answer:";
+
+  if (!number1 || !number2 || operator === "?") {
+    calculatorOutput.textContent = "Please enter a valid calculation";
+  } else if (currentOperator.textContent == "+") {
     result = number1 + number2;
     calculatorOutput.textContent += ` ${result}`;
-  } else if (operator == "-") {
+  } else if (currentOperator.textContent == "-") {
     result = number1 - number2;
     calculatorOutput.textContent += ` ${result}`;
-  } else if (operator == "*") {
+  } else if (currentOperator.textContent == "*") {
     result = number1 * number2;
     calculatorOutput.textContent += ` ${result}`;
-  } else if (operator == "/") {
+  } else if (currentOperator.textContent == "/") {
     result = number1 / number2;
     calculatorOutput.textContent += ` ${result}`;
+  } else {
+    calculatorOutput.textContent = "Please enter a valid calculation";
   }
 });
 
 calculatorReset.addEventListener("click", () => {
   inputNumber1.value = "";
   inputNumber2.value = "";
-  inputOperator.value = "";
+  currentOperator.textContent = "?";
   calculatorOutput.textContent = "Answer:";
 });
