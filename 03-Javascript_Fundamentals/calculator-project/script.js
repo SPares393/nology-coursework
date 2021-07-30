e47602a36eb0f6834b0bda542a9398e013112677
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-07-30 08:45:03 +0100

    added list for previous calculations

--- 03-Javascript_Fundamentals/calculator-project/script.js
+++ 03-Javascript_Fundamentals/calculator-project/script.js
@@ -1,12 +1,7 @@
-// TO DO:
-
-// Change if/else statement to a switch statement
-// Put input into an array? Or running total?
-
 const calculatorInput = document.querySelector(".calculator-input");
 const number = document.querySelectorAll(".number");
 const operator = document.querySelectorAll(".operator");
-const calculateTotal = document.querySelector(".calculator-total");
+const calculatorTotal = document.querySelector(".calculator-total");
 const calculatorReset = document.querySelector(".calculator-reset");
 
 let total = 0;
@@ -19,9 +14,34 @@ for (let i = 0; i < number.length; i++) {
 
 for (let i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", () => {
-    calculatorInput.value += operator[i].textContent;
+    calculatorInput.value += ` ${operator[i].textContent} `;
   });
 }
+
+calculatorTotal.addEventListener("click", () => {
+  const sumArr = calculatorInput.value.split(" ");
+  console.log(sumArr);
+  if (sumArr.length === 1) {
+    console.log("Invalid calculation");
+  } else if (sumArr.length === 3) {
+    const firstNumber = Number(sumArr[0]);
+    const secondNumber = Number(sumArr[2]);
+    let result;
+    if (sumArr[1] === "+") {
+      result = firstNumber + secondNumber;
+      calculatorInput.value = result;
+    } else if (sumArr[1] === "-") {
+      result = firstNumber - secondNumber;
+      calculatorInput.value = result;
+    } else if (sumArr[1] === "*") {
+      result = firstNumber * secondNumber;
+      calculatorInput.value = result;
+    } else if (sumArr[1] === "/") {
+      result = firstNumber / secondNumber;
+      calculatorInput.value = result;
+    }
+  }
+});
 
 calculatorReset.addEventListener("click", () => {
   total = 0;
