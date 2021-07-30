6b988b127cbf7e0de2c20b1b5f147a81caa89531
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-07-30 13:10:35 +0100

    accounted for invalid calculations

--- 03-Javascript_Fundamentals/calculator-project/script.js
+++ 03-Javascript_Fundamentals/calculator-project/script.js
@@ -5,93 +5,69 @@ const calculatorTotal = document.querySelector(".calculator-total");
 const calculatorReset = document.querySelector(".calculator-reset");
 const calculationList = document.querySelector(".calculation-list");
 
+let total = 0;
+let hasResult = false;
+
 for (let i = 0; i < number.length; i++) {
   number[i].addEventListener("click", () => {
-    calculatorInput.value += number[i].textContent;
+    if (hasResult === true) {
+      calculatorInput.value = number[i].textContent;
+      hasResult = false;
+    } else {
+      calculatorInput.value += number[i].textContent;
+    }
   });
 }
 
 for (let i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", () => {
-    calculatorInput.value += ` ${operator[i].textContent} `;
+    if (calculatorInput.value && !hasResult) {
+      calculatorInput.value += ` ${operator[i].textContent} `;
+    } else {
+      calculatorInput.value = "";
+    }
   });
 }
 
 const addToList = function (arr, sumResult) {
-  calculationList.innerHTML += `<li>${arr[0]} ${arr[1]} ${arr[2]} = ${sumResult}</li>`;
+  calculationList.innerHTML += `<li> ${arr.join(" ")} = ${sumResult}</li>`;
 };
 
 calculatorTotal.addEventListener("click", () => {
   const sumArr = calculatorInput.value.split(" ");
   console.log(sumArr);
   if (sumArr.length === 1) {
-    console.log("Invalid calculation");
-  } else if (sumArr.length === 3) {
-    const firstNumber = Number(sumArr[0]);
-    const secondNumber = Number(sumArr[2]);
-    let result;
-    if (sumArr[1] === "+") {
-      result = firstNumber + secondNumber;
-      calculatorInput.value = result;
-      addToList(sumArr, result);
-    } else if (sumArr[1] === "-") {
-      result = firstNumber - secondNumber;
-      calculatorInput.value = result;
-      addToList(sumArr, result);
-    } else if (sumArr[1] === "*") {
-      result = firstNumber * secondNumber;
-      calculatorInput.value = result;
-      addToList(sumArr, result);
-    } else if (sumArr[1] === "/") {
-      result = firstNumber / secondNumber;
-      calculatorInput.value = result;
-      addToList(sumArr, result);
-    }
-  } else if (sumArr.length > 3) {
-    let total = 0;
-    const firstNumber = Number(sumArr[0]);
-    const secondNumber = Number(sumArr[2]);
-    let result;
-    if (sumArr[1] === "+") {
-      result = firstNumber + secondNumber;
-      total += result;
-      calculatorInput.value = total;
-      console.log(total);
-    } else if (sumArr[1] === "-") {
-      result = firstNumber - secondNumber;
-      total += result;
-      calculatorInput.value = total;
-    } else if (sumArr[1] === "*") {
-      result = firstNumber * secondNumber;
-      total += result;
-      calculatorInput.value = total;
-    } else if (sumArr[1] === "/") {
-      result = firstNumber / secondNumber;
-      total += result;
-      calculatorInput.value = total;
-    }
-
-    for (let i = 3; i < sumArr.length; i++) {
-      if (i === "+") {
-        total += sumArr[i + 1];
-        console.log(total);
-        calculatorInput.value = total;
-      } else if (i === "-") {
-        total -= sumArr[i + 1];
-        console.log(total);
-        calculatorInput.value = total;
-      } else if (i === "*") {
-        total *= sumArr[i + 1];
-        console.log(total);
-        calculatorInput.value = total;
-      } else if (i === "/") {
-        total /= sumArr[i + 1];
-        console.log(total);
-        calculatorInput.value = total;
-      } else {
+    calculatorInput.value = sumArr[0];
+  } else if (sumArr.length >= 3) {
+    total = Number(sumArr[0]);
+    for (let i = 0; i < sumArr.length; i++) {
+      if (i % 2 === 0) {
         continue;
+      } else if (
+        !parseInt(sumArr[i]) &&
+        sumArr[i + 1] === "" &&
+        !parseInt(sumArr[i + 2])
+      ) {
+        calculatorInput.value = "error";
+        break;
+      } else {
+        if (sumArr[i] === "+") {
+          total += Number(sumArr[i + 1]);
+          calculatorInput.value = total;
+        } else if (sumArr[i] === "-") {
+          total -= Number(sumArr[i + 1]);
+          calculatorInput.value = total;
+        } else if (sumArr[i] === "*") {
+          total *= Number(sumArr[i + 1]);
+          calculatorInput.value = total;
+        } else if (sumArr[i] === "/") {
+          total /= Number(sumArr[i + 1]);
+          calculatorInput.value = total;
+        }
       }
+      addToList(sumArr, total);
     }
+    hasResult = true;
   }
 });
 
