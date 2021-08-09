87fc6eae18744b8a725f10949f84a21a72fb3713
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-08-09 11:53:37 +0100

    arrays of complex objects

--- 04-Javascript_Advanced/looping-complex-data/arraysOfObjects.js
+++ 04-Javascript_Advanced/looping-complex-data/arraysOfObjects.js
@@ -1,35 +1,76 @@
+// ----- SIMPLE OBJECTS -----
+
+// const fruitObjectsArray = [
+//   { fruit: "apple" },
+//   { fruit: "banana" },
+//   { fruit: "orange" },
+// ];
+
+// // Log banana to the console
+// // console.log(fruitObjectsArray[1].fruit);
+
+// // Make fruit array
+// let fruits = fruitObjectsArray.map((fruitObject) => {
+//   const fruitValue = fruitObject.fruit;
+//   return fruitValue;
+// });
+// // console.log(fruits);
+
+// const searchTerm = "apple";
+
+// let matchingFruitArrray = fruitObjectsArray.filter((fruitObject) => {
+//   const fruitMatch = fruitObject.fruit.includes(searchTerm);
+//   return fruitMatch;
+// });
+// // console.log(matchingFruitArrray);
+
+// // Create a function that takes an array of fruit
+// // Map over array and genrate some <li> tags for the fruit
+// // Return a string of the <li> tags
+// const generateFruitTags = (fruitArray) => {
+//   const html = fruitArray.map((fruitObject) => {
+//     return `<li>${fruitObject.fruit}</li>`;
+//   });
+
+//   return html.join("");
+// };
+// // console.log(generateFruitTags(fruitObjectsArray));
+
+// ----- COMPLEX OBJECTS -----
+
 const fruitObjectsArray = [
-  { fruit: "apple" },
-  { fruit: "banana" },
-  { fruit: "orange" },
+  { fruit: "apple", rating: 8, price: 150 },
+  { fruit: "banana", rating: 5, price: 100 },
+  { fruit: "orange", rating: 6, price: 120 },
 ];
 
-// Log banana to the console
-// console.log(fruitObjectsArray[1].fruit);
+// console.log(fruitObjectsArray[2].rating);
 
-// Make fruit array
-let fruits = fruitObjectsArray.map((fruitObject) => {
-  const fruitValue = fruitObject.fruit;
-  return fruitValue;
-});
-// console.log(fruits);
-
-const searchTerm = "apple";
-
-let matchingFruitArrray = fruitObjectsArray.filter((fruitObject) => {
-  const fruitMatch = fruitObject.fruit.includes(searchTerm);
-  return fruitMatch;
-});
-// console.log(matchingFruitArrray);
-
-// Create a function that takes an array of fruit
-// Map over array and genrate some <li> tags for the fruit
-// Return a string of the <li> tags
-const generateFruitTags = (fruitArray) => {
+const generateFruitHtml = (fruitArray) => {
   const html = fruitArray.map((fruitObject) => {
-    return `<li>${fruitObject.fruit}</li>`;
+    const capitalised =
+      fruitObject.fruit[0].toUpperCase() + fruitObject.fruit.substring(1);
+
+    const price = (fruitObject.price / 100).toFixed(2);
+
+    const cardHtml = `
+    <div>
+        <h3>${capitalised}</h3>
+        <p>This fruit is ${fruitObject.rating}/10</p>
+        <p>Buy now for £${price}</p>
+    </div>
+    `;
+    return cardHtml;
   });
 
   return html.join("");
 };
-console.log(generateFruitTags(fruitObjectsArray));
+
+// Sort the objects by rating (highest first)
+
+const sortByhighestRating = fruitObjectsArray.sort((a, b) => {
+  return b.rating - a.rating;
+});
+
+console.log(sortByhighestRating);
+console.log(generateFruitHtml(sortByhighestRating));
