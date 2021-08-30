5687a529da102823686a1bf40b6a0b0a0e351a60
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-08-30 14:05:49 +0100

    query selectors added - ship classes created

--- 04-Javascript_Advanced/hostile-aliens/script.js
+++ 04-Javascript_Advanced/hostile-aliens/script.js
@@ -1,5 +1,5 @@
 class Ship {
-  constructor(hitPoints, hitDamage) {
+  constructor(name, hitPoints, hitDamage) {
     this.hitPoints = hitPoints;
     this.hitDamage = hitDamage;
   }
@@ -10,8 +10,21 @@ class Ship {
 }
 
 class MotherShip extends Ship {
-  constructor(hitPoints, hitDamage) {
-    super(hitPoints, hitDamage);
+  constructor(name, hitPoints, hitDamage) {
+    super(name, hitPoints, hitDamage);
   }
-  destroyAll() {}
+  destroy() {}
 }
+
+const attackShip = new Ship("Attack Ship", 45, 12);
+const defenceShip = new Ship("Defence Ship", 80, 10);
+const theMotherShip = new MotherShip("The Mother Ship", 100, 9);
+
+const gameArea = document.querySelector(".game-area");
+const buttonFire = document.querySelector(".button-fire");
+
+const createShipHTML = (shipClass) => {
+  return `<div class="ship"><h3>${shipClass.name}</h3><p class="hit-points">Hit Points: ${shipClass.hitPoints}</p></div>`;
+};
+
+const gameInit = () => {};
