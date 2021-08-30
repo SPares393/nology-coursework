66ff009e932eb9437cea383959220194b2c39cbd
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-08-30 14:28:06 +0100

    gameInit function and basic ship styling

--- 04-Javascript_Advanced/hostile-aliens/script.js
+++ 04-Javascript_Advanced/hostile-aliens/script.js
@@ -1,7 +1,9 @@
 class Ship {
-  constructor(name, hitPoints, hitDamage) {
+  constructor(name, hitPoints, hitDamage, quantity) {
+    this.name = name;
     this.hitPoints = hitPoints;
     this.hitDamage = hitDamage;
+    this.quantity = quantity;
   }
   takeDamage() {
     this.hitPoints = this.hitPoints - this.hitDamage;
@@ -16,15 +18,28 @@ class MotherShip extends Ship {
   destroy() {}
 }
 
-const attackShip = new Ship("Attack Ship", 45, 12);
-const defenceShip = new Ship("Defence Ship", 80, 10);
-const theMotherShip = new MotherShip("The Mother Ship", 100, 9);
+const attackShip = new Ship("Attack Ship", 45, 12, 8);
+const defenceShip = new Ship("Defence Ship", 80, 10, 5);
+const theMotherShip = new MotherShip("The Mother Ship", 100, 9, 1);
 
 const gameArea = document.querySelector(".game-area");
 const buttonFire = document.querySelector(".button-fire");
 
 const createShipHTML = (shipClass) => {
-  return `<div class="ship"><h3>${shipClass.name}</h3><p class="hit-points">Hit Points: ${shipClass.hitPoints}</p></div>`;
+  //   let shipHMTL = "";
+  //   for (let i = 1; i < shipClass.quantity; i++) {
+  //   shipHMTL +=
+  return `<div class="ship"><h3>${shipClass.name}</h3><p class="hit-points">Hit Points: <span>${shipClass.hitPoints}</span></p></div>`;
+  //   }
 };
 
-const gameInit = () => {};
+const gameInit = () => {
+  gameArea.innerHTML = `<div class="row-1"></div><div class="row-2"></div><div class="row-3"></div>`;
+  document.querySelector(".row-1").innerHTML = createShipHTML(theMotherShip);
+  for (let i = 0; i < 5; i++) {
+    document.querySelector(".row-2").innerHTML += createShipHTML(defenceShip);
+  }
+  for (let i = 0; i < 8; i++) {
+    document.querySelector(".row-3").innerHTML += createShipHTML(attackShip);
+  }
+};
