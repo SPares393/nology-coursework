c5b4dd2c38b6461196d9220d360b2223837b21a5
Stuart Pares <stuartpares@live.co.ukgit config --global user.email stuartpares@live.co.ukclearcleargit config --global user.name stuartpares@live.co.uk>
2021-09-07 15:45:23 +0100

    list styling & toggleHideList function

--- 02-Web-Fundamentals/lions-rugby-project/dist/script.dev.js
+++ 02-Web-Fundamentals/lions-rugby-project/dist/script.dev.js
@@ -2,7 +2,8 @@
 
 var playerList = document.querySelector(".player-list");
 var player = document.querySelectorAll(".player");
-var mainCard = document.querySelectorAll(".main__card"); // prettier-ignore
+var mainCard = document.querySelectorAll(".main__card");
+var listCloseButton = document.querySelector(".list-close-button"); // prettier-ignore
 
 var playerArr = [["Alun-Wyn Jones", [4, 5], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Josh Adams", [11, 14, 15], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Bundi Aki", [12, 13], "🇮🇪"], ["Adam Beard", [4, 5], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Tadhg Beirne", [4, 5, 6], "🇮🇪"], ["Dan Biggar", [10], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Jack Conan", [8], "🇮🇪"], ["Luke Cowan-Dickie", [2], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Tom Curry", [6, 7, 8], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Elliot Daly", [11, 12, 13, 14, 15], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Gareth Davies", [9], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Zander Fagerson", [3], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"], ["Taulupe Faletau", [8], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Owen Farrell", [10, 12], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Tadhg Furlong", [3], "🇮🇪"], ["Jamie George", [2], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Chris Harris", [12, 13], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"], ["Iain Henderson", [4, 5], "🇮🇪"], ["Robbie Henshaw", [12, 13], "🇮🇪"], ["Jonny Hill", [4, 5], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Stuart Hogg", [15], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"], ["Maro Itoje", [4, 5, 6], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Wyn Jones", [1], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Ronan Kelleher", [2], "🇮🇪"], ["Courtney Lawes", [4, 5, 6], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Duhan van-der-Merwe", [11, 14], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"], ["Conor Murray", [9], "🇮🇪"], ["Josh Navidi", [6, 7], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Ken Owens", [2], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Ali Price", [9], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"], ["Louis Rees-Zammit", [11, 14], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"], ["Finn Russell", [10], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"], ["Sam Simmonds", [8], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Kyle Sinckler", [3], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Marcus Smith", [10], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Rory Sutherland", [1], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"], ["Mako Vunipola", [1], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Hamish Watson", [6, 7], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"], ["Anthony Watson", [11, 14, 15], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"], ["Liam Williams", [11, 14, 15], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"]];
 
@@ -27,6 +28,10 @@ var fillPlayerList = function fillPlayerList(playerArr) {
 
 var currentCard;
 
+var toggleHideList = function toggleHideList() {
+  playerList.classList.contains("hidden") ? playerList.classList.remove("hidden") : playerList.classList.add("hidden");
+};
+
 for (var i = 0; i < mainCard.length; i++) {
   mainCard[i].addEventListener("click", function () {
     currentCard = this;
@@ -38,7 +43,7 @@ for (var i = 0; i < mainCard.length; i++) {
     }
 
     this.classList += " active";
-    playerList.classList.remove("hidden");
+    toggleHideList();
     var position = parseInt(this.querySelector(".card__number").textContent);
     var availablePlayers = playerFilterByPosition(position);
     var availablePlayersList = fillPlayerList(availablePlayers);
@@ -46,6 +51,9 @@ for (var i = 0; i < mainCard.length; i++) {
     for (var _i = 0; _i < availablePlayersList.length; _i++) {
       playerList.innerHTML += availablePlayersList[_i];
     }
+
+    playerList.innerHTML += "<button class=\"list-close-button\">Close</button>";
+    listCloseButton.addEventListener("click", toggleHideList());
   });
 }
 
@@ -54,7 +62,7 @@ playerList.addEventListener("click", function (e) {
     var playerInfo = e.target.textContent.split(" ");
     var cardFillHTML = "<p class=\"card__name\">".concat(playerInfo[0], "<br>").concat(playerInfo[1], "</p><h3 class=\"card__flag\">").concat(playerInfo[2], "</h3>");
     currentCard.insertAdjacentHTML("beforeend", cardFillHTML);
-    playerList.classList.add("hidden");
+    toggleHideList();
   }
 }); // NEXT STEPS
 // Remove player from the player list if they're already on a card
--- 02-Web-Fundamentals/lions-rugby-project/script.js
+++ 02-Web-Fundamentals/lions-rugby-project/script.js
@@ -1,6 +1,7 @@
 const playerList = document.querySelector(".player-list");
 const player = document.querySelectorAll(".player");
 const mainCard = document.querySelectorAll(".main__card");
+const listCloseButton = document.querySelector(".list-close-button");
 
 // prettier-ignore
 const playerArr = [["Alun-Wyn Jones", [4, 5], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Josh Adams", [11, 14, 15], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Bundi Aki", [12, 13], "🇮🇪"],["Adam Beard", [4, 5], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Tadhg Beirne", [4, 5, 6], "🇮🇪"],["Dan Biggar", [10], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Jack Conan", [8], "🇮🇪"],["Luke Cowan-Dickie", [2], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Tom Curry", [6, 7, 8], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Elliot Daly", [11, 12, 13, 14, 15], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Gareth Davies", [9], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Zander Fagerson", [3], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"],["Taulupe Faletau", [8], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Owen Farrell", [10, 12], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Tadhg Furlong", [3], "🇮🇪"],["Jamie George", [2], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Chris Harris", [12, 13], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"],["Iain Henderson", [4, 5], "🇮🇪"],["Robbie Henshaw", [12, 13], "🇮🇪"],["Jonny Hill", [4, 5], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Stuart Hogg", [15], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"],["Maro Itoje", [4, 5, 6], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Wyn Jones", [1], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Ronan Kelleher", [2], "🇮🇪"],["Courtney Lawes", [4, 5, 6], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Duhan van-der-Merwe", [11, 14], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"],["Conor Murray", [9], "🇮🇪"],["Josh Navidi", [6, 7], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Ken Owens", [2], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Ali Price", [9], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"],["Louis Rees-Zammit", [11, 14], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],["Finn Russell", [10], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"],["Sam Simmonds", [8], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Kyle Sinckler", [3], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Marcus Smith", [10], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Rory Sutherland", [1], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"],["Mako Vunipola", [1], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Hamish Watson", [6, 7], "🏴󠁧󠁢󠁳󠁣󠁴󠁿"],["Anthony Watson", [11, 14, 15], "🏴󠁧󠁢󠁥󠁮󠁧󠁿"],["Liam Williams", [11, 14, 15], "🏴󠁧󠁢󠁷󠁬󠁳󠁿"],];
@@ -24,6 +25,12 @@ const fillPlayerList = (playerArr) => {
 
 let currentCard;
 
+const toggleHideList = () => {
+  playerList.classList.contains("hidden")
+    ? playerList.classList.remove("hidden")
+    : playerList.classList.add("hidden");
+};
+
 for (let i = 0; i < mainCard.length; i++) {
   mainCard[i].addEventListener("click", function () {
     currentCard = this;
@@ -36,7 +43,7 @@ for (let i = 0; i < mainCard.length; i++) {
     }
     this.classList += " active";
 
-    playerList.classList.remove("hidden");
+    toggleHideList();
 
     const position = parseInt(this.querySelector(".card__number").textContent);
     const availablePlayers = playerFilterByPosition(position);
@@ -45,6 +52,9 @@ for (let i = 0; i < mainCard.length; i++) {
     for (let i = 0; i < availablePlayersList.length; i++) {
       playerList.innerHTML += availablePlayersList[i];
     }
+    playerList.innerHTML += `<button class="list-close-button">Close</button>`;
+
+    listCloseButton.addEventListener("click", toggleHideList());
   });
 }
 
@@ -53,8 +63,7 @@ playerList.addEventListener("click", (e) => {
     const playerInfo = e.target.textContent.split(" ");
     const cardFillHTML = `<p class="card__name">${playerInfo[0]}<br>${playerInfo[1]}</p><h3 class="card__flag">${playerInfo[2]}</h3>`;
     currentCard.insertAdjacentHTML("beforeend", cardFillHTML);
-
-    playerList.classList.add("hidden");
+    toggleHideList();
   }
 });
 
